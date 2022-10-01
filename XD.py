537d9cb96f7a2cb85c0b7a7ccbc7e3640231489d
datacirexyen <llonix29@gmail.com>
2022-10-01 18:41:54 -0300

    third

--- XD.py
+++ XD.py
@@ -13,16 +13,19 @@ html_string = '''
   <head><title>HTML Pandas Dataframe with CSS</title></head>
   <link rel="stylesheet" type="text/css" href="df_style.css"/>
   <body>
-  <h1>Result:</h1>
+  <h1>Stock/Crypto portfolio</h1>
+  <p>if you want to help the project please consider donating, BCH and matic friendly!</p>
+  <h1>Here are your results</h1>
   <div class="Cen">
     {table}
-  </div>
- 
-  <h1 class="TItulo">Graph</h1>
+  <img src="{{url_for('static', filename='polygon-matic.gif.gif')}}" />
+ <div class="GraphCont">
+  <h2 class="TItulo">Graph</h2>
   <img src='my_plot.png'/>
-  
+  </div>
+  </div>
   <footer>
-    
+    <a class="Links" href="{{url_for('donate')}}">Donate!</a>
     <a class="Links" href="https://twitter.com/LilElseCaller">Twitter</a>
     <a class="Links" href="https://medium.com/@LilElseCaller">Medium</a>
     <a class="Links" href="https://github.com/egarcia00">Algo Creator</a></p>
@@ -74,16 +77,20 @@ def download(tickers, start=None, end=None, actions=False, threads=True,
             If not None stops waiting for a response after given number of
             seconds. (Can also be a fraction of a second e.g. 0.01)
     """
+portfolio_ =[]
 app = Flask(__name__)
 
+
+   
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     if request.method == 'POST':
         a=request.form .get('longitude')
         benchmark_ = ["^GSPC",]
         #portfolio_ = ['ADA-USD', 'BTC-USD','ETH-USD',"SOL-USD","XRP-USD"]
 
-        portfolio_ =[]
+        
         print('Welcome to the portfolio optimizer')
         print( 'Enter each token in XXX-USD format Example: ETH-USD ADA-USD')
         print("///////////////////////////////////////////////////")
@@ -92,136 +99,149 @@ def index():
         portfolio_ = [item for item in a.split()]
 
         print(portfolio_)
+        portlen=len(portfolio_)
         
+      
+            
+               
+            
+            
             
 
 
+        if (portlen>=1):
+                start_date_ = "2020-01-01"
+                end_date_  = "2022-01-31"
+                number_of_scenarios = 10000
 
-        start_date_ = "2020-01-01"
-        end_date_  = "2022-01-31"
-        number_of_scenarios = 10000
+                return_vector = []
+                risk_vector = []
+                distrib_vector = []
 
-        return_vector = []
-        risk_vector = []
-        distrib_vector = []
+                #Get Information from Benchmark and Portfolio
+                df = yf.download(benchmark_, start=start_date_, end=end_date_)
 
-        #Get Information from Benchmark and Portfolio
-        df = yf.download(benchmark_, start=start_date_, end=end_date_)
+                for t in portfolio_:
+                    Stock=yf.Ticker(t)
+                    if (Stock.info['regularMarketPrice'] == None):
+                            #raise NameError("You did not input a correct stock ticker! Try again.")
+                            portfolio_.remove(t)
+                            print(portfolio_)
+                    else:
+                            print("DONE!")
+                
+                df2 = yf.download(portfolio_, start=start_date_, end=end_date_)
 
-        for t in portfolio_:
-            Stock=yf.Ticker(t)
-            if (Stock.info['regularMarketPrice'] == None):
-                    #raise NameError("You did not input a correct stock ticker! Try again.")
-                    portfolio_.remove(t)
-                    print(portfolio_)
-            else:
-                    print("DONE!")
-        
-        df2 = yf.download(portfolio_, start=start_date_, end=end_date_)
+                #Clean Rows with No Values on both Benchmark and Portfolio
+                df = df.dropna(axis=0)
+                df2 = df2.dropna(axis=0)
 
-        #Clean Rows with No Values on both Benchmark and Portfolio
-        df = df.dropna(axis=0)
-        df2 = df2.dropna(axis=0)
+                #Matching the Days
+                df = df[df.index.isin(df2.index)]
+                df.head()
 
-        #Matching the Days
-        df = df[df.index.isin(df2.index)]
-        df.head()
+                # Analysis of Benchmark
+                benchmark_vector = np.array(df['Close'])
 
-        # Analysis of Benchmark
-        benchmark_vector = np.array(df['Close'])
+                #Create our Daily Returns
+                benchmark_vector = np.diff(benchmark_vector)/benchmark_vector[1:]
 
-        #Create our Daily Returns
-        benchmark_vector = np.diff(benchmark_vector)/benchmark_vector[1:]
+                #Select or Final Return and Risk
+                benchmark_return = np.average(benchmark_vector)
+                benchmark_risk = np.std(benchmark_vector)
 
-        #Select or Final Return and Risk
-        benchmark_return = np.average(benchmark_vector)
-        benchmark_risk = np.std(benchmark_vector)
+                #Add our Benchmark info to our lists
+                return_vector.append(benchmark_return)
+                risk_vector.append(benchmark_risk)
 
-        #Add our Benchmark info to our lists
-        return_vector.append(benchmark_return)
-        risk_vector.append(benchmark_risk)
+                # Analysis of Portfolio
+                portfolio_vector = np.array(df2['Close'])
 
-        # Analysis of Portfolio
-        portfolio_vector = np.array(df2['Close'])
+                #Create a loop for the number of scenarios we want:
 
-        #Create a loop for the number of scenarios we want:
-
-        for i in range(number_of_scenarios):
-            #Create a random distribution that sums 1 
-            # and is split by the number of stocks in the portfolio
-            random_distribution = np.random.dirichlet(np.ones(len(portfolio_)),size=1)
-            distrib_vector.append(random_distribution)
-            
-            #Find the Closing Price for everyday of the portfolio
-            portfolio_matmul = np.matmul(random_distribution,portfolio_vector.T)
-            
-            #Calculate the daily return
-            portfolio_matmul = np.diff(portfolio_matmul)/portfolio_matmul[:,1:]
-            
-            #Select or Final Return and Risk
-            portfolio_return = np.average(portfolio_matmul, axis=1)
-            portfolio_risk = np.std(portfolio_matmul, axis=1)
-            
-            #Add our Benchmark info to our lists
-            return_vector.append(portfolio_return[0])
-            risk_vector.append(portfolio_risk[0])
-
-            #Create Risk Boundaries
-        delta_risk = 0.05
-        min_risk = np.min(risk_vector)
-        max_risk = risk_vector[0]*(1+delta_risk)
-        risk_gap = [min_risk, max_risk]
-
-        portfolio_array = np.column_stack((return_vector,risk_vector))[1:,]
-
-        # Rule to create the best portfolio
-        # If the criteria of minimum risk is satisfied then:
-        if np.where(((portfolio_array[:,1]<= max_risk)))[0].shape[0]>1:
-            min_risk_portfolio = np.where(((portfolio_array[:,1]<= max_risk)))[0]
-            best_portfolio_loc = portfolio_array[min_risk_portfolio]
-            max_loc = np.argmax(best_portfolio_loc[:,0])
-            best_portfolio = best_portfolio_loc[max_loc]
-
-        # If the criteria of minimum risk is not satisfied then:
-        else:
-            min_risk_portfolio = np.where(((portfolio_array[:,1]== np.min(risk_vector[1:]))))[0]
-            best_portfolio_loc = portfolio_array[min_risk_portfolio]
-            max_loc = np.argmax(best_portfolio_loc[:,0])
-            best_portfolio = best_portfolio_loc[max_loc]
-        #Visual Representation
-        trade_days_per_year = 252
-        risk_gap = np.array(risk_gap)*trade_days_per_year
-        best_portfolio[0] = np.array(best_portfolio[0])*trade_days_per_year
-
-
-       
+                for i in range(number_of_scenarios):
+                    #Create a random distribution that sums 1 
+                    # and is split by the number of stocks in the portfolio
+                    random_distribution = np.random.dirichlet(np.ones(len(portfolio_)),size=1)
+                    distrib_vector.append(random_distribution)
                     
-        #Output Table of Distributions
-        portfolio_loc = np.where((portfolio_array[:,0]==(best_portfolio[0]/trade_days_per_year))&(portfolio_array[:,1]==(best_portfolio[1])))[0][0]
-        best_distribution = distrib_vector[portfolio_loc][0].tolist()
-        d = {"Ticker": portfolio_, "Asset % in Portfolio": best_distribution}
-        output = pd.DataFrame(d)
-        
-        output = output.sort_values(by=["Asset % in Portfolio"],ascending=False)
-        output=output.set_index("Ticker")
+                    #Find the Closing Price for everyday of the portfolio
+                    portfolio_matmul = np.matmul(random_distribution,portfolio_vector.T)
+                    
+                    #Calculate the daily return
+                    portfolio_matmul = np.diff(portfolio_matmul)/portfolio_matmul[:,1:]
+                    
+                    #Select or Final Return and Risk
+                    portfolio_return = np.average(portfolio_matmul, axis=1)
+                    portfolio_risk = np.std(portfolio_matmul, axis=1)
+                    
+                    #Add our Benchmark info to our lists
+                    return_vector.append(portfolio_return[0])
+                    risk_vector.append(portfolio_risk[0])
 
-       
-        graph=output.plot(kind="pie",y="Asset % in Portfolio", figsize=(5, 5)).figure
-        
-        graph.savefig('my_plot.png',format="png",facecolor="#2C3639" , transparent=True)
-        output= output.style.format({"Asset % in Portfolio": "{:.2%}"})
-        
-        
+                    #Create Risk Boundaries
+                delta_risk = 0.05
+                min_risk = np.min(risk_vector)
+                max_risk = risk_vector[0]*(1+delta_risk)
+                risk_gap = [min_risk, max_risk]
 
-        with open('str.html', 'w') as f:
-            f.write(html_string.format(table=output.to_html(classes='mystyle')))
+                portfolio_array = np.column_stack((return_vector,risk_vector))[1:,]
 
-        filename = 'str.html'
-        webbrowser.open_new_tab(filename)           
-        
+                # Rule to create the best portfolio
+                # If the criteria of minimum risk is satisfied then:
+                if np.where(((portfolio_array[:,1]<= max_risk)))[0].shape[0]>1:
+                    min_risk_portfolio = np.where(((portfolio_array[:,1]<= max_risk)))[0]
+                    best_portfolio_loc = portfolio_array[min_risk_portfolio]
+                    max_loc = np.argmax(best_portfolio_loc[:,0])
+                    best_portfolio = best_portfolio_loc[max_loc]
+
+                # If the criteria of minimum risk is not satisfied then:
+                else:
+                    min_risk_portfolio = np.where(((portfolio_array[:,1]== np.min(risk_vector[1:]))))[0]
+                    best_portfolio_loc = portfolio_array[min_risk_portfolio]
+                    max_loc = np.argmax(best_portfolio_loc[:,0])
+                    best_portfolio = best_portfolio_loc[max_loc]
+                #Visual Representation
+                trade_days_per_year = 252
+                risk_gap = np.array(risk_gap)*trade_days_per_year
+                best_portfolio[0] = np.array(best_portfolio[0])*trade_days_per_year
+
+
+            
+                            
+                #Output Table of Distributions
+                portfolio_loc = np.where((portfolio_array[:,0]==(best_portfolio[0]/trade_days_per_year))&(portfolio_array[:,1]==(best_portfolio[1])))[0][0]
+                best_distribution = distrib_vector[portfolio_loc][0].tolist()
+                d = {"Ticker": portfolio_, "Asset % in Portfolio": best_distribution}
+                output = pd.DataFrame(d)
+                
+                output = output.sort_values(by=["Asset % in Portfolio"],ascending=False)
+                output=output.set_index("Ticker")
+                output.columns.name = output.index.name
+                output.index.name = None
+            
+                graph=output.plot(kind="pie",y="Asset % in Portfolio", figsize=(5, 5)).figure
+                
+                graph.savefig('my_plot.png',format="png",facecolor="#2C3639" , transparent=True)
+                output= output.style.format({"Asset % in Portfolio": "{:.2%}"})
+                
+                
+
+                with open(f'templates/{portfolio_}.html', 'w') as f:
+                    f.write(html_string.format(table=output.to_html(classes='mystyle')))
+
+                filename = f'{portfolio_}.html' 
+                webbrowser.open_new_tab(f"/{portfolio_}")
+    else:
+        print("Pusiste una sola accion o una huevada asi, hacer un screen")
     return render_template('index.html')
 
+
+
+@app.route("/donate")
+def donate():
+    return render_template('Donations.html')
+        
 if __name__ == '__main__':
     app.run()
 
-    
